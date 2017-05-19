292f2191c42c04d173f0ae134bbf4fcb55550c5e
eiNjel <alekw3@gmail.com>
2017-05-19 13:58:56 +0200

    ui patch

--- Minifier.sh
+++ Minifier.sh
@@ -4,15 +4,15 @@
 #############################################################
 
 rm -f web/static/js/init/controllers.min.js
-uglifyjs web/static/js/init/controllers.js -o web/static/js/init/controllers.min.js  -p 5 -c -m
+uglifyjs web/static/js/init/controllers.js -o web/static/js/init/controllers.min.js -c -m
 rm -f web/static/js/init/main.min.js
-uglifyjs web/static/js/init/main.js -o web/static/js/init/main.min.js  -p 5 -c -m
+uglifyjs web/static/js/init/main.js -o web/static/js/init/main.min.js -c -m
 #rm -f web/static/js/init/routes.min.js
 #uglifyjs web/static/js/init/routes.js -o web/static/js/init/routes.min.js  -p 5 -c -m
 rm -f web/static/js/init/services.min.js
-uglifyjs web/static/js/init/services.js -o web/static/js/init/services.min.js  -p 5 -c -m
+uglifyjs web/static/js/init/services.js -o web/static/js/init/services.min.js -c -m
 rm -f web/static/js/js-main.min.js
-uglifyjs web/static/js/js-main.js -o web/static/js/js-main.min.js  -p 5 -c -m
+uglifyjs web/static/js/js-main.js -o web/static/js/js-main.min.js -c -m
 rm -f web/static/style/css/main.min.css
 uglifycss web/static/style/css/main.css > web/static/style/css/main.min.css
 rm -f web/static/style/css/responsive.min.css
